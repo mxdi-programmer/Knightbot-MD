cbaf89d2460a6dc1a1d3298ebd7368154bc31933
mxdi-programmer <maxigerald139@gmail.com>
2026-05-04 23:31:27 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight Bot',
-  author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  author: '‎♰𝚕𝚎ͥ𝚐𝚎ͣ𝚗ͫ𝚍✁♡ʜᴇᴀʀᴛʙʀᴇᴀᴋᴇʀ꧂',
+  botName: "♰𝚕𝚎ͥ𝚐𝚎ͣ𝚗ͫ𝚍✁♡ʜᴇᴀʀᴛʙʀᴇᴀᴋᴇʀ꧂",
+  botOwner: '♰𝚕𝚎ͥ𝚐𝚎ͣ𝚗ͫ𝚍✁♡ʜᴇᴀʀᴛʙʀᴇᴀᴋᴇʀ꧂', // Your name
+  ownerNumber: '2349010758061', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
