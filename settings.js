540358e06a72b55e207cd1b21e8d590221623797
mxdi-programmer <maxigerald139@gmail.com>
2026-05-15 03:35:38 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ const settings = {
   botOwner: '♰𝚕𝚎ͥ𝚐𝚎ͣ𝚗ͫ𝚍✁♡ʜᴇᴀʀᴛʙʀᴇᴀᴋᴇʀ꧂', // Your name
   ownerNumber: '2349010758061', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  commandMode: "public",
+  commandMode: "private",
   maxStoreMessages: 20, 
   storeWriteInterval: 10000,
   description: "This is a bot for managing group commands and automating tasks.",
